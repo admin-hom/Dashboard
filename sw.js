dc55374d6df5a293565d3c3545015616c82a76dd
admin-hom <rahmatnobil.rn@gmail.com>
2026-06-10 09:23:18 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,25 @@
-const CACHE = 'myapps-v1';
+const CACHE = 'myapps-v2';
 const ASSETS = [
-  '/index.html',
-  '/manifest.json'
+  '/Dashboard/index.html',
+  '/Dashboard/manifest.json'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
+self.addEventListener('activate', e=>{
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    caches.keys().then(keys=>
+      Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', e=>{
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(r=>r||fetch(e.request))
   );
 });
